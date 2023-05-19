2bb1274df7f0f3ae9b30b79951e7a85b92bbbcb8
samuel carrizo <samuelcarrizot@gmail.com>
2023-05-19 20:13:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyForm from './components/Form';
+import nave from '../public/nave.jpg'
+
+
+const App = () => {
+    return (
+        <main className='app'>
+                <div className="skewed"></div>
+            <MyForm />
+        </main>
+    )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,60 @@
+import React, { useState ,useRef } from 'react';
+import Swal from 'sweetalert2';
+const MyForm = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [input2Value, setInput2Value] = useState('');
+    const [result ,setResult] = useState('');
+    const activeInput = useRef(null);
+
+
+    const calculator = () => {
+        const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
+        return nums.map((item) => {
+            return (
+                <button key={item} type="button" onClick={() => handleNumButtonClick(item)} className='button_cal'>
+                    {item}
+                </button>
+            );
+        });
+    };
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+    const handleInput2Change = (event) => {
+        setInput2Value(event.target.value);
+    };
+    const handleResultClick = (event) => {
+        event.preventDefault();
+        const result = (parseFloat(inputValue) * parseFloat(input2Value)) / 100;
+        setResult(result);
+    };
+
+    const handleNumButtonClick = (num) => {
+        if (activeInput.current === 'input1') {
+            setInputValue((prevValue) => prevValue + num);
+        } else if (activeInput.current === 'input2') {
+            setInput2Value((prevValue) => prevValue + num);
+        }
+    };
+
+    const handleInputFocus = (inputName) => {
+        activeInput.current = inputName;
+    };
+    return (
+        <main>
+            <form className='container_form'>
+            <h1>TIP CALCULATOR</h1>
+                <input placeholder='Enter Account' type="number" value={inputValue} onChange={handleInputChange} onFocus={() => handleInputFocus('input1')} />
+                <input placeholder='Enter Percentage' type="number" value={input2Value} onChange={handleInput2Change} onFocus={() => handleInputFocus('input2')} />
+                <div className='nums_container'>
+                {calculator()}
+                </div>
+                <button className='b_result' onClick={handleResultClick}>RESULT</button>
+            </form>
+            <section className='result'><strong>{result}</strong></section>
+        </main>
+    );
+};
+
+export default MyForm;
